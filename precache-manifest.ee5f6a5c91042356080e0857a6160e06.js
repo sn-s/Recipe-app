4417c3df10e8af30cfe1a176f45556416133cbca
5teve5 <47039007+5teve5@users.noreply.github.com>
2019-09-29 22:37:53 +0100

    Updates

---
+++ precache-manifest.ee5f6a5c91042356080e0857a6160e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cac76d3a2d617fb321a70a38bca767",
+    "url": "/Recipe-app/index.html"
+  },
+  {
+    "revision": "c316565366d5f66167ab",
+    "url": "/Recipe-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a7c770c1981d7a1342da",
+    "url": "/Recipe-app/static/css/main.38c4e1c5.chunk.css"
+  },
+  {
+    "revision": "c316565366d5f66167ab",
+    "url": "/Recipe-app/static/js/2.d9fd6c52.chunk.js"
+  },
+  {
+    "revision": "a7c770c1981d7a1342da",
+    "url": "/Recipe-app/static/js/main.30559a3f.chunk.js"
+  },
+  {
+    "revision": "182a6d6796f271f84e75",
+    "url": "/Recipe-app/static/js/runtime~main.d807f277.js"
+  }
+]);
